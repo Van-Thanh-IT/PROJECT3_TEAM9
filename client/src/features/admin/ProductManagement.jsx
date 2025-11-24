ef2f98a26840639895f988331d051be1398b9505
Van-Thanh-IT <lovanthanh34523@gmail.com>
2025-11-24 22:16:55 +0700

    push

---
+++ client/src/features/admin/ProductManagement.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductManagement = () => {
+    return (
+        <div>
+            <h1>Chức năng quản lý sản phẩm</h1>
+        </div>
+    );
+}
+
+export default ProductManagement;
---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppRoutes = () => {
+    return (
+        <div>
+            <h1>Route</h1>
+        </div>
+    );
+}
+
+export default AppRoutes;
